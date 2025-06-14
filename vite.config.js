c51de6030ddd4128035f74a59575710b5f4dc7f6
dandevs-git <danilojrabancia@gmail.com>
2025-06-14 21:28:01 +0800

    dsawdsfawfdwa

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     laravel({
-      input: ["resources/css/main.css", "resources/js/main.jsx"],
+      input: ["resources/js/main.jsx"],
       refresh: true,
     }),
     react(),
   ],
-  server: {
-    host: "localhost",
-    port: 5173,
-  },
-  build: {
-    manifest: true,
-    outDir: "public/build",
-    emptyOutDir: true,
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom", "axios"],
-        },
-      },
-    },
-  },
 });
