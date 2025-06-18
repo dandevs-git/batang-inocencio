66a6cb9e80ff944f9926227b357a0df905895bb5
dandevs-git <danilojrabancia@gmail.com>
2025-06-18 11:30:29 +0800

    sadwdwad

--- config/cors.php
+++ config/cors.php
@@ -21,8 +21,8 @@ return [
 
     // 'allowed_origins' => ['*'],
     'allowed_origins' => [
-        'http://127.0.0.1:8000',
-        'http://localhost:8000',
+        // 'http://127.0.0.1:8000',
+        // 'http://localhost:8000',
         'https://trecebatanginocencio.site',
     ],
 
